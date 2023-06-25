e07d3ec9931b85ec4f57da676fc36119b5beba08
scanbeu <136927001+scanbeu@users.noreply.github.com>
2023-06-24 20:44:08 -0400

    Update terms-of-use.php

--- terms-of-use.php
+++ terms-of-use.php
@@ -5,24 +5,24 @@
 		<title>Terms Of Use | COMP1002 HTML, CSS, and JS Fundamentals</title>
 		<meta name="author" content="The name of the document’s author to go here">
 		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-		<link rel="stylesheet" href="../css/styles.css" />
+		<link rel="shortcut icon" href="favicon.png" type="x-icon">
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 	<body id="section6">
 		<!-- Page-level header -->
 		<header>
-			<img src="../images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<img src="html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 			<!-- Global site navigation -->
 			<nav>
 				<ul>
-					<li><a href="../index.php" title="Go to the Home page">Home</a></li>
-					<li><a href="../red.php" title="Learn about red blocks">Red Blocks</a></li>
-					<li><a href="../blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
-					<li><a href="../yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
-					<li><a href="../green.php" title="Learn about green blocks">Green Blocks</a></li>
-					<li><a href="../about.php" title="Learn about pages"> About pages</a></li>
+					<li><a href="index.php" title="Go to the Home page">Home</a></li>
+					<li><a href="red.php" title="Learn about red blocks">Red Blocks</a></li>
+					<li><a href="blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+					<li><a href="yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+					<li><a href="green.php" title="Learn about green blocks">Green Blocks</a></li>
+					<li><a href="about.php" title="Learn about pages"> About pages</a></li>
 				</ul>
 			</nav>
 		</header>
