e401c7346a814a69beb0067f06693707627570a4
scanbeu <136927001+scanbeu@users.noreply.github.com>
2023-06-24 20:34:15 -0400

    Update about.php

--- about.php
+++ about.php
@@ -5,13 +5,13 @@
 		<title>About | COMP1002 HTML, CSS, and JS Fundamentals</title>
 		<meta name="author" content="The name of the document’s author to go here">
 		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="shortcut icon" href="favicon.png" type="x-icon">
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 	<body id="section6">
 		<!-- Page-level header -->
 		<header>
-			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<img src="html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 			<!-- Global site navigation -->
@@ -40,9 +40,9 @@
 		<!-- Page-level footer -->
 		<footer>
 			<nav>
-				<a href="about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | 
-				<a href="about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> |
-				<a href="about/terms-of-use.php#limitation-liability" title="Learn about Limitation of Liability">Limitation of Liability</a>
+				<a href="privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | 
+				<a href="privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> |
+				<a href="terms-of-use.php#limitation-liability" title="Learn about Limitation of Liability">Limitation of Liability</a>
 			</nav>
 			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
 		</footer>
