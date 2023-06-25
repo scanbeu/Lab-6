0908d073663cb890675c0f9ca3ea50cf517b9f4a
scanbeu <136927001+scanbeu@users.noreply.github.com>
2023-06-24 20:42:41 -0400

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -5,13 +5,13 @@
 		<title>Subscribe | COMP1002 HTML, CSS, and JS Fundamentals</title>
 		<meta name="author" content="The name of the document’s author to go here" />
 		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="shortcut icon" href="favicon.png" type="x-icon" />
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 	<body>
 		<!-- Page-level header -->
 		<header>
-			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<img src="html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 		</header>
